c669b6cc17d1610f58396fe2f35a80b6143c31a6
andriideikalo <wlada801@gmail.com>
2023-02-15 21:19:52 +0200

    Create API.js

---
+++ src/components/API.js
@@ -0,0 +1,7 @@
+// key = e0f7258397e08a39ddd2202708092bf4;
+
+// приклад запиту
+//https://api.themoviedb.org/3/movie/550?api_key=e0f7258397e08a39ddd2202708092bf4
+
+//Ключ доступа к API (v4 auth)
+//eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMGY3MjU4Mzk3ZTA4YTM5ZGRkMjIwMjcwODA5MmJmNCIsInN1YiI6IjYzZWQyYjA1MWYzZTYwMDA3OWUwMmNiMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5mEAOCzzhMgYe4Ej0AWizREJMHX-hL3oq5A7BIV8yqA
