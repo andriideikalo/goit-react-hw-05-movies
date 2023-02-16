28e619a24f1569c8001f18543feccc124d45fecc
andriideikalo <wlada801@gmail.com>
2023-02-16 14:10:32 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from 'path/to/pages/Home';
+import About from 'path/to/pages/About';
+import Products from 'path/to/pages/Products';
 
+export const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </div>
+  );
+};
