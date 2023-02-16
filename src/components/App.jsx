77a60dca83f15c65009419df4802267ef2391493
andriideikalo <wlada801@gmail.com>
2023-02-16 15:33:01 +0200

    add api link

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from 'path/to/pages/Home';
-import About from 'path/to/pages/About';
-import Products from 'path/to/pages/Products';
+import Home from 'pages/Home';
+import Movies from 'pages/Movies';
+import MovieDetalis from 'pages/MovieDetails';
+import Cast from 'pages/Cast';
+import Reviews from 'pages/Reviews';
+import getTrending from './API';
 
 export const App = () => {
+  getTrending();
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetalis />} />
+        <Route path="/movies/:movieId/cast" element={<Cast />} />
+        <Route path="/movies/:movieId/reviews" element={<Reviews />} />
       </Routes>
     </div>
   );
