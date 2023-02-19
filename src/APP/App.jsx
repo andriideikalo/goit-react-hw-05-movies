75d34e98a964a35c4a4f9da4d2f10ce6ba043247
andriideikalo <wlada801@gmail.com>
2023-02-19 14:24:09 +0200

    fix

--- src/APP/App.jsx
+++ src/APP/App.jsx
@@ -2,13 +2,13 @@ import { lazy } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { SharedLayout } from 'components/SharedLayout';
 
-const Home = lazy(() => import('./pages/Home'));
-const Movies = lazy(() => import('./pages/Movies'));
-const MovieDetails = lazy(() => import('./pages/MovieDetails'));
-const Cast = lazy(() => import('./components/Cast'));
-const Reviews = lazy(() => import('./components/Reviews'));
+const Home = lazy(() => import('../pages/Home'));
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
+const Cast = lazy(() => import('../components/Cast'));
+const Reviews = lazy(() => import('../components/Reviews'));
 
-export const App = () => {
+export default function App() {
   return (
     <div>
       <Routes>
@@ -24,4 +24,4 @@ export const App = () => {
       </Routes>
     </div>
   );
-};
+}
