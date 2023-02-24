dc0f0e5490e05ad0be204a737bff05a7b4132757
andriideikalo <wlada801@gmail.com>
2023-02-24 18:52:51 +0200

    add backdrop_path

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -6,15 +6,23 @@ export const MovieList = ({ items }) => {
   const location = useLocation();
 
   return (
-    <ul>
-      {items.map(({ id, title }) => (
-        <li key={id}>
+    <styl.Wraper>
+      {items.map(({ id, title, backdrop_path }) => (
+        <styl.List key={id}>
           <styl.Item to={`/movies/${id}`} state={{ from: location }}>
             <styl.Name>{title}</styl.Name>
+            <img
+              src={
+                backdrop_path
+                  ? `https://image.tmdb.org/t/p/w500${backdrop_path}`
+                  : 'https://svgsilh.com/svg/2153523-9e9e9e.svg'
+              }
+              alt={'name'}
+            />
           </styl.Item>
-        </li>
+        </styl.List>
       ))}
-    </ul>
+    </styl.Wraper>
   );
 };
 
--- src/components/MovieList/MovieList.styled.jsx
+++ src/components/MovieList/MovieList.styled.jsx
@@ -2,6 +2,8 @@ import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Item = styled(NavLink)`
+  display: block;
+  margin: 0 auto;
   transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1),
     color 250ms cubic-bezier(0.4, 0, 0.2, 1);
 
@@ -12,4 +14,21 @@ export const Item = styled(NavLink)`
 `;
 export const Name = styled.p`
   font-size: 16px;
+  margin-bottom: 8px;
+`;
+
+export const Wraper = styled.ul`
+  display: grid;
+  grid: repeat(7, 140px) / auto-flow 300px;
+  grid-gap: 100px;
+`;
+export const List = styled.li`
+  margin-bottom: 0;
+  display: grid;
+  text-align: center;
+`;
+export const Img = styled.img`
+  width: 140px;
+  height: 140px;
+  display: grid;
 `;
