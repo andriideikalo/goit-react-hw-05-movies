1c79a2a4ef7bb02cd48c649a9894de6eefaf2c48
andriideikalo <wlada801@gmail.com>
2023-02-24 17:00:50 +0200

    fix location

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
 import * as styl from './MovieList.styled';
+import { useLocation } from 'react-router-dom';
 
 export const MovieList = ({ items }) => {
+  const location = useLocation();
+
   return (
     <ul>
       {items.map(({ id, title }) => (
         <li key={id}>
-          <styl.Item to={`/movies/${id}`}>
+          <styl.Item to={`/movies/${id}`} state={{ from: location }}>
             <styl.Name>{title}</styl.Name>
           </styl.Item>
         </li>
